c685cdf4c86595ab4fe2c91c0b36d66314237337
BFKazmierczak <bfkazmierczak@gmail.com>
2024-01-28 21:57:28 +0100

    fix: enable graphql

---
+++ config/plugins.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  graphql: {
+    enabled: true,
+    config: {
+      playgroundAlways: false,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
+});
