5613870aa1d890e87d1a488b76ef3847ed963dcb
Bartłomiej Kaźmierczak <105592983+BFKazmierczak@users.noreply.github.com>
2024-01-29 11:42:32 +0100

    fix: try to fix ffmpeg enoent

--- src/extensions/upload/services/index.ts
+++ src/extensions/upload/services/index.ts
@@ -4,7 +4,7 @@ const ffprobe = require("ffprobe-static");
 
 export default {
   calculateDuration: async (file) => {
-    ffmpeg.setFfprobePath(ffprobe.path);
+    // ffmpeg.setFfprobePath(ffprobe.path);
 
     const rootDir = path.resolve(__dirname, "../../../../..");
     const filePath = path.join(rootDir, "public", file.url);
@@ -12,7 +12,7 @@ export default {
     console.log("FILEPATH:", filePath);
 
     return new Promise((resolve, reject) => {
-      ffmpeg.ffprobe(filePath, (err, metadata) => {
+      ffprobe(filePath, (err, metadata) => {
         if (err) reject(err);
         else {
           const durationInSeconds = metadata.format.duration;
