cc55440575baebb8fae231fcf9436b73f02b2173
Bartłomiej Kaźmierczak <105592983+BFKazmierczak@users.noreply.github.com>
2024-01-29 11:52:20 +0100

    fix: set path to ffmpeg

--- src/extensions/upload/services/index.ts
+++ src/extensions/upload/services/index.ts
@@ -1,10 +1,12 @@
 const path = require("path");
 const ffmpeg = require("fluent-ffmpeg");
-const ffprobe = require("ffprobe-static");
+const ffmpegStatic = require("ffmpeg-static");
+const ffprobeStatic = require("ffprobe-static");
 
 export default {
   calculateDuration: async (file) => {
-    // ffmpeg.setFfprobePath(ffprobe.path);
+    ffmpeg.setFfmpegPath(ffmpegStatic.path);
+    ffmpeg.setFfprobePath(ffprobeStatic.path);
 
     const rootDir = path.resolve(__dirname, "../../../../..");
     const filePath = path.join(rootDir, "public", file.url);
@@ -12,7 +14,7 @@ export default {
     console.log("FILEPATH:", filePath);
 
     return new Promise((resolve, reject) => {
-      ffprobe(filePath, (err, metadata) => {
+      ffmpeg.ffprobe(filePath, (err, metadata) => {
         if (err) reject(err);
         else {
           const durationInSeconds = metadata.format.duration;
